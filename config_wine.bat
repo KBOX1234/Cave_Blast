1e4a39459736055e4d7e4bc6d0b368f561fa4216
KBOX1234 <KBOXKBOX1234@gmail.com>
2025-05-25 16:02:17 +0200

    wine config

---
+++ config_wine.bat
@@ -0,0 +1,44 @@
+set PATH=%PATH%;C:\Program Files\w64-devkit\bin\
+
+
+mkdir lib
+
+cd ./external/rl_imgui/
+
+chmod +x premake5
+
+./premake5 gmake
+
+make
+
+cd ../..
+
+cp ./external/rl_imgui/bin/Debug/rlImGui.lib ./lib/rlImGui.lib
+
+cd ./external/raylib/src
+
+make PLATFORM=PLATFORM_DESKTOP
+
+cd ../../..
+
+cp ./external/raylib/src/libraylib.a ./lib/libraylib.a
+
+cd ./external/enet/
+
+cmake -G "MinGW Makefiles" CMakeLists.txt --fresh
+
+make
+
+cd ../../
+
+cp ./external/enet/libenet.a ./lib/libenet.a
+
+cp ./make_windows ./Makefile
+
+cd ./external/sphysics/
+
+make
+
+cd ../../
+
+cp ./external/sphysics/libsphysics.a ./lib/libsphysics.a
