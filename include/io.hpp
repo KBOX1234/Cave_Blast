52bf452531b542009eb7f9332a2a54ef68adcd24
Luke Richard Stanghelle <kboxkbox1234@gmail.com>
2025-08-06 19:21:21 +0200

    working on io

--- include/io.hpp
+++ include/io.hpp
@@ -1,14 +1,34 @@
+#pragma once
 #include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
 
 #define NOFILE "iul84y0ufp"
+#define STRERROR "hdlslonfe"
 
 class easy_file_ops{
     public:
         static std::string load_text_file(std::string filename);
 
         static int save_to_text_file(std::string data, std::string filename);
+};
 
-        static char* load_binary_file(std::string filename);
+class save_master{
+    private:
+        std::string save_name;
 
-        static void save_binary_file(char* data, size_t size, std::string filename);
+        std::string save_dir;
+
+        std::string get_home_dir();
+
+        std::string get_save_dir();
+
+    public:
+        save_master();
+
+        int save_chunk(std::string json_data);
+
+        int save_player(std::string json_data);
 };
--- src/io.cpp
+++ src/io.cpp
@@ -2,6 +2,12 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <cstdlib>
+#include <filesystem>
+#include "json.hpp"
+using json = nlohmann::json;
+namespace fs = std::filesystem;
+
 
 std::string easy_file_ops::load_text_file(std::string filename){
     std::ifstream file(filename);
@@ -30,4 +36,44 @@ int easy_file_ops::save_to_text_file(std::string data, std::string filename){
     file.close();
 
     return 0;
+}
+
+std::string save_master::get_home_dir(){
+    #ifdef _WIN32
+        const char* home = std::getenv("HOMEPATH");
+    #else 
+        const char* home = std::getenv("HOME");
+    #endif
+
+    if(home == nullptr) return STRERROR;
+
+    return home;
+}
+
+std::string save_master::get_save_dir(std::string save_name){
+    std::string home = get_home_dir();
+
+    if(home == STRERROR){
+        return STRERROR;
+    }
+
+    std::string save_dir = home + "/" + save_name;
+
+    return save_dir;
+}
+
+save_master::save_master(){
+    save_name = "dev";
+
+    save_dir = get_save_dir(save_name);
+}
+
+#define CHUNK_DIR "chunks"
+
+int save_master::save_chunk(std::string json_data){
+    std::string chnk_save_name;
+
+    json chunk_json = json::parse(json_data);
+
+    
 }
